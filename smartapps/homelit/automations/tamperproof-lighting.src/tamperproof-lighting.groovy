721915ef98fec5b68b1150741709e119d90ba31d
ISAAC BERGMAN <aniceberg@gmail.com>
2018-05-13 19:23:24 -0400

    Initial commit

---
+++ smartapps/homelit/automations/tamperproof-lighting.src/tamperproof-lighting.groovy
@@ -0,0 +1,272 @@
+definition(
+    name: "Tamperproof Lighting",
+    namespace: "homeLit/automations",
+    author: "iceberg",
+    description: "A simple app to create tamperproof lighting schedules. This is a child app.",
+    category: "My Apps",
+
+    // the parent option allows you to specify the parent app in the form <namespace>/<app name>
+    parent: "homeLit/parent:Tamperproof Lighting",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    page name: "mainPage", title: "Automate Lights & Switches", install: false, uninstall: true, nextPage: "namePage"
+    page name: "namePage", title: "Automate Lights & Switches", install: true, uninstall: true
+    page name: "timeSetPage", title: "How to trigger the switch"
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unschedule()
+    initialize()
+}
+
+def initialize() {
+    // if the user did not override the label, set the label to the default
+    if (!overrideLabel) {
+        app.updateLabel(defaultLabel())
+    }
+    // schedule the turn on and turn off handlers
+    schedule(scheduleStartTime, startHandler)
+    schedule(scheduleEndTime, endHandler)
+}
+
+// main page to select lights, the action, and turn on/off times
+def mainPage() {
+    dynamicPage(name: "mainPage") {
+        section {
+            lightInputs()
+            actionInputs()
+        	timeInputs()
+            tamperProtection()
+            switchOverrides()
+            motionOverrides()
+        }
+        
+    }
+}
+
+// page for allowing the user to give the automation a custom name
+def namePage() {
+    if (!overrideLabel) {
+        // if the user selects to not change the label, give a default label
+        def l = defaultLabel()
+        log.debug "will set default label of $l"
+        app.updateLabel(l)
+    }
+    dynamicPage(name: "namePage") {
+        if (overrideLabel) {
+            section("Automation name") {
+                label title: "Enter custom name", defaultValue: app.label, required: false
+            }
+        } else {
+            section("Automation name") {
+                paragraph app.label
+            }
+        }
+        section {
+            input "overrideLabel", "bool", title: "Edit automation name", defaultValue: "false", required: "false", submitOnChange: true
+        }
+    }
+}
+
+// inputs to select the lights
+def lightInputs() {
+    input "lights", "capability.switch", title: "Which switches do you want to control?", multiple: true, submitOnChange: true
+}
+
+// inputs to control what to do with the lights (turn on/keep on, turn off/keep off)
+def actionInputs() {
+    if (lights) {
+    	input "action", "enum", title: "What do you want to do?", options: ["off":"Turn off", "on":"Turn on"], required: true, submitOnChange: true
+    }
+}
+
+// inputs for selecting on and off time
+def timeInputs() {
+    if (settings.action) {
+    	section {
+	    	href "timeSetPage", title: "When do you want to schedule the switches?"
+        }      
+	}
+    def scheduleStartTime = "toggleStartTime"
+    def scheduleEndTime = "toggleEndTime"
+    
+}
+
+// page for selecting automation triggers
+def timeSetPage() {
+	dynamicPage(name: "timeSetPage") {
+    	section ("By time") {
+            input "timeStartOptions", "enum", title: "When to schedule switch ${action}?", multiple: false, options: ["sunrise":"At sunrise", "sunset":"At sunset", "manualTime":"At a specified time"], submitOnChange: true
+                if (timeStartOptions == "manualTime") {
+                    input "toggleStartTime", "time", title: "Time to toggle switch ${action}", required: true
+                } else {
+                	section("Location") {
+    					input "autoLocation", "bool", title: "Enable automatic detection:", defaultValue: true, submitOnChange: true
+        				if (autoLocation=="false") {
+				        	input"locationZIP", "number", title: "Enter zipcode:", required: true, defaultValue: "11223", range: "0..99999"
+       					}
+    				}
+                	input "sOffsetStartTime", "number", title: "+/- minutes from ${timeStartOptions}. (e.g. Enter '-30' for half hour before ${timeStartOptions})", defaultValue: "0", range: "-719..719", required = false
+                }
+			input "timeEndOptions", "enum", title: "When to end ${action} schedule?", multiple: false, options: ["sunrise":"At sunrise", "sunset":"At sunset", "manualTime":"At a specified time"], submitOnChange: true
+            	if (timeEndOptions == "manualTime") {
+                    input "toggleEndTime", "time", title: "Time to toggle switch ${action}", required: true
+                } else {
+                	input "sOffsetEndTime", "number", title: "Optional: Choose +/- minutes from ${timeEndOptions}. (e.g. Enter '-30' for half hour before ${timeEndOptions})", defaultValue: "0", range: "-719..719", required: true
+                }
+			}
+	
+		section ("By specific mode") {
+			input "modes", "mode", title: "Only when mode is", multiple: true, required: false, submitOnChange: true
+    	}
+	}
+}
+
+def tamperProtection() {
+	section ("Tamper Protection") {
+    	input "tamperProtection", "bool", title: "Prevent switches from being toggled during schedule?",  defaultValue: "false", required: "false", submitOnChange: true
+        if (tamperProtection) {
+           	input "desiredState", "enum", title: "Prevent switches from being...", multiple: false, options: ["off":"turned off", "on":"turned on"], required: true, submitOnChange: true
+           	if (lights.find{it.hasCommand('tapUp2')} != null) {
+        		switchOverrides()
+           	}
+   		}
+	}
+}
+
+def switchOverrides() {
+	input "switchOverrideCmd", "enum", title: "Override tamper protection with double, triple or long hold?", 
+	options: ["false":"No", "tapUp2":"Double-Tap Up", "tapDown2":"Double-Tap Down", "tapUp3":"Triple-Tap Up", "tapDown3":"Triple-Tap Down", "holdUp":"Hold Up", "holdDown":"Hold Down"],  defaultValue: "No", required: false
+}
+
+def motionOverrides() {
+	section ("Motion Detection Rules") {
+        input "motion", "capability.motionSensor", title: "Which motion sensors should be checked for activity?", multiple: true, required: "false"
+    }
+}
+
+// the handler method that turns and keeps the lights ON 
+def startHandler() {
+    // switch on the selected action
+    switch(action) {
+        case "on":
+            log.debug "on()"
+            lights.on()
+            break
+		case "off":
+            log.debug "off()"
+            lights.off()
+            break
+        }
+}
+
+// the handler method that turns and keeps the lights OFF 
+def endHandler() {
+	switch(action) {
+        case "on":
+            log.debug "End on() Schedule"
+            lights.on()
+            break
+		case "off":
+            log.debug "End off() Schedule"
+            lights.off()
+            break
+	}
+}
+
+// a method that will set the default label of the automation.
+// It uses the lights selected and action to create the automation label
+def defaultLabel() {
+    def lightsLabel = settings.lights.size() == 1 ? lights[0].displayName : lights[0].displayName + ", etc..."
+        "Turn $action $lightsLabel"
+}
+
+// utility method to get a map of available actions for the selected switches
+def actionMap() {
+    def map = [on: "Turn On", off: "Turn Off"]
+    if (lights.find{it.hasCommand('setLevel')} != null) {
+        map.level = "Turn On & Set Level"
+    }
+    if (lights.find{it.hasCommand('setColor')} != null) {
+        map.color = "Turn On & Set Color"
+    }
+    map
+}
+
+// utility method to collect the action map entries into maps for the input
+def actionOptions() {
+    actionMap().collect{[(it.key): it.value]}
+}
+
+// set the color and level as specified, if the user selected to set color.
+def setColor() {
+
+    def hueColor = 0
+    def saturation = 100
+
+    switch(color) {
+            case "White":
+            hueColor = 52
+            saturation = 19
+            break;
+        case "Daylight":
+            hueColor = 53
+            saturation = 91
+            break;
+        case "Soft White":
+            hueColor = 23
+            saturation = 56
+            break;
+        case "Warm White":
+            hueColor = 20
+            saturation = 80
+            break;
+        case "Blue":
+            hueColor = 70
+            break;
+        case "Green":
+            hueColor = 39
+            break;
+        case "Yellow":
+            hueColor = 25
+            break;
+        case "Orange":
+            hueColor = 10
+            break;
+        case "Purple":
+            hueColor = 75
+            break;
+        case "Pink":
+            hueColor = 83
+            break;
+        case "Red":
+            hueColor = 100
+            break;
+    }
+
+    def value = [switch: "on", hue: hueColor, saturation: saturation, level: level as Integer ?: 100]
+    log.debug "color = $value"
+
+    lights.each {
+        if (it.hasCommand('setColor')) {
+            log.debug "$it.displayName, setColor($value)"
+            it.setColor(value)
+        } else if (it.hasCommand('setLevel')) {
+            log.debug "$it.displayName, setLevel($value)"
+            it.setLevel(level as Integer ?: 100)
+        } else {
+            log.debug "$it.displayName, on()"
+            it.on()
+        }
+    }
+}
---
+++ smartapps/homelit/parent/tamperproof-lighting.src/tamperproof-lighting.groovy
@@ -0,0 +1,56 @@
+/**
+ *  Copyright 2015 SmartThings
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Keeps automated light schedules from being manually tampered
+ *  
+ *  Author: iceberg
+ */
+
+definition(
+    name: "Tamperproof Lighting",
+    namespace: "homeLit/parent",
+    author: "iceberg",
+    description: "Smart Lighting with Tamper Protection (Parent app)",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+    // The parent app preferences are pretty simple: just use the app input for the child app.
+    page(name: "mainPage", title: "Tamperproof Lighting", install: true, uninstall: true,submitOnChange: true) {
+        section {
+            app(name: "tamperproofLighting", appName: "Tamperproof Lighting", namespace: "homeLit/automations", title: "Create New Schedule", multiple: true)
+            }
+    }
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    // nothing needed here, since the child apps will handle preferences/subscriptions
+    // this just logs some messages for demo/information purposes
+    log.debug "there are ${childApps.size()} child smartapps"
+    childApps.each {child ->
+        log.debug "child app: ${child.label}"
+    }
+}
